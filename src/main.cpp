50ac96799c16cffc0a372462bcbef93ca43996a8
ohhaase <oscarhaase@me.com>
2026-03-12 16:15:53 -0500

    GLFW window creation

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,82 @@
+#define VULKAN_HPP_NO_STRUCT_CONSTRUCTORS
+#define GLFW_INCLUDE_VULKAN
+#include <vulkan/vulkan.hpp>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+constexpr uint32_t WIDTH = 800;
+constexpr uint32_t HEIGHT = 600;
+
+class HelloTriangleApplication
+{
+    public:
+        void run()
+        {
+            initWindow();
+            initVulkan();
+            mainLoop();
+            cleanup();
+        }
+
+    private:
+        GLFWwindow* window;
+
+        void initWindow()
+        {
+            // Initialize GLFW
+            glfwInit();
+
+            // Tell GLFW to not use OpenGL nor allow resizability (for now)
+            glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+            glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+            // Create window
+            window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+        }
+
+        void initVulkan()
+        {
+
+        }
+
+        void mainLoop()
+        {
+            // Loop until told to close
+            while (!glfwWindowShouldClose(window))
+            {
+                glfwPollEvents();
+            }
+        }
+
+        void cleanup()
+        {
+            // The tutorial I'm following uses RAII, so this will not change anymore.
+            // Usually you would have to remove all the Vulkan things at the end here.
+            
+            // Destroy window
+            glfwDestroyWindow(window);
+            
+            // Terminate GLFW
+            glfwTerminate();
+        }
+};
+
 
 int main()
 {
-    std::cout << "Hello world!";
-    return 0;
+    try
+    {
+        HelloTriangleApplication app;
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
 }
